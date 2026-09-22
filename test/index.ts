f299c11de7e26948dc5d968d6416ded3223aad06
nichoth <nichoth@nichoth.com>
2026-09-22 09:16:26 -0700

    setup the tests

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,9 @@
 import { test } from '@substrate-system/tapzero'
-import { example } from '../src/index.js'
+import { waitFor } from '@substrate-system/dom'
 
 test('example', async t => {
+    t.plan(2)
     t.ok('ok', 'should be an example')
-    example()
+    document.body.innerHTML += '<div></div>'
+    t.ok(await waitFor('div'), 'a div exists')
 })
